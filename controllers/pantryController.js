e3d7c99ff5a7bc1c5e988c53664b6c008d119069
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-26 11:54:18 -0600

    Basic add ingredient functionality

--- controllers/pantryController.js
+++ controllers/pantryController.js
@@ -1,6 +1,9 @@
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, UnauthenticatedError } = require('../errors')
 const Pantry = require('../models/Pantry')
+const User = require('../models/User')
+
+const Ingredient = require('../models/Ingredient')
 
 const getAllIngredients = async (req,res) => {
     const queryObject = {
@@ -18,6 +21,34 @@ const getIngredient = async (req, res) => {
   res.send('getIngredient')
 }
 const addIngredient = async (req, res) => {
+  //res.send('createIngredient')
+
+  const userID = '63cf4154cd27e45d2cb5c38a'
+  const searchIngredientID = '63cdacc67eb0e3588cbf2d38'
+
+  const queryObject = {
+    // Use authenticaiton to change to req.user.userID
+    ownedBy: '63cf4154cd27e45d2cb5c38a'
+  }
+
+  let user = await User.findById(userID)
+  let ingredient = await Ingredient.findById(searchIngredientID)
+
+  let result = Pantry.find(queryObject)
+  let pantry = await result
+
+
+  
+  console.log(user);
+  //console.log(pantry);
+
+  const pantryIngredients = pantry[0].ingredients
+
+  pantry[0].ingredients.push(ingredient)
+
+  console.log(pantryIngredients);
+  //console.log(ingredient);
+
   res.send('createIngredient')
 }
 // module.exports = getAllIngredients
