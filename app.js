aea4a3185c1de0175066389da43824d1f9461966
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-01-30 09:47:43 -0600

    Deleted Swagger documentation

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 require('dotenv').config()
 require('express-async-errors');
 
-
-// Swagger 
-const swaggerUI = require('swagger-ui-express')
-const YAML = require('yamljs')
-const swaggerDocument = YAML.load('./swagger.yaml')
-
 const express = require('express')
 app = express()
 const connectDB = require('./db/connect')
@@ -31,13 +25,8 @@ app.use(errorHandlerMiddleware);
 // Database
 
 app.get('/', (req, res) => {
-    res.send('<h1>SimplKitchenAPI</h1><a href="/api-docs">Documentation</a>')
+    res.send('<h1>SimplKitchenAPI</h1>')
 })
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
-
-// app.get('/', (req, res) => {
-//     res.send('Hello world')
-// })
 
 const port = process.env.PORT || 3000;
 
