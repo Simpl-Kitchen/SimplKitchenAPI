7c720e764bd85b08917b82fc0e8baf05487e712c
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-02-01 17:27:28 -0600

    Updated ingredients Model

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,9 +1,41 @@
 const mongoose = require('mongoose')
 
 const IngredientSchema = new mongoose.Schema({
-    name: {
+    foodId: {
         type: String,
-        required: [true, 'please provide ingredient name'],
+        required: [true, 'please provide the food Id'],
+    },
+    label: {
+        type: String,
+        required: [true, 'please provide the name of the ingredient']
+
+    },
+    brand: {
+        type: String,
+
+    },
+    category: {
+        type: String,
+        enum: ['generic-foods', 'packaged-foods', 'generic-meals', 'fast-foods'],
+    },
+    categoryLabel: {
+        type: Number,
+        enum: ['food', 'meal'],
+        default: 'food'
+    },
+    foodContentsLabel: {
+        type: String
+    },
+    image: {
+        type: String,
+    },
+    amount: {
+        type: Number
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide a user']
     }
 }, { strict: false })
 
