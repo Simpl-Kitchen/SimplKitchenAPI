34abb61b86464504cb3443281b8340a52e531439
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-02-12 16:38:38 -0600

    Added comments

--- controllers/pantryController.js
+++ controllers/pantryController.js
@@ -10,14 +10,14 @@ const getAllIngredients = async (req, res) => {
     let result = Ingredient.find(queryObject)
     const ingredients = await result
 
-    res.status(StatusCodes.OK).json({ingredients})
+    res.status(StatusCodes.OK).json({ ingredients })
     //console.log(queryObject)
     //res.send("Get all ingredients")
 }
 const getIngredient = async (req, res) => {
     const {
-        user: {userId},
-        params: {id: ingredientId}
+        user: { userId },
+        params: { id: ingredientId }
     } = req
 
     const ingredient = await Ingredient.findOne({
@@ -28,7 +28,7 @@ const getIngredient = async (req, res) => {
         throw new NotFoundError(`No ingredient with ${ingredientId}`)
 
     }
-    res.status(StatusCodes.OK).json({ingredient})
+    res.status(StatusCodes.OK).json({ ingredient })
     //res.send("Get ingredient")
 }
 const addIngredient = async (req, res) => {
@@ -36,34 +36,36 @@ const addIngredient = async (req, res) => {
     console.log(req.user)
     req.body.createdBy = req.user.userId
     const ingredient = await Ingredient.create(req.body)
-    res.status(StatusCodes.CREATED).json({ingredient})
-    //res.send("Add Ingredient")
+    res.status(StatusCodes.CREATED).json({ ingredient })
+
+
+    // Regenerate recipes
 }
 const updateIngredient = async (req, res) => {
     const {
-        body: {amount},
-        user: {userId},
-        params: {id: ingredientId}
+        body: { amount },
+        user: { userId },
+        params: { id: ingredientId }
     } = req
 
     if (amount === '') {
         throw new BadRequestError('Amount field cannot be empty')
     }
     const ingredient = await Ingredient.findByIdAndUpdate(
-        {_id: ingredientId, createdBy: userId},
+        { _id: ingredientId, createdBy: userId },
         req.body,
-        {new: true, runValidators: true}
+        { new: true, runValidators: true }
     )
     if (!ingredient) {
         throw new NotFoundError(`No ingredient with id ${ingredientId}`)
     }
-    res.status(StatusCodes.OK).json({ingredient})
+    res.status(StatusCodes.OK).json({ ingredient })
     //res.send("Update Ingredient")
 }
 const deleteIngredient = async (req, res) => {
     const {
-        user: {userId},
-        params: {id: ingredientId}
+        user: { userId },
+        params: { id: ingredientId }
     } = req
 
     const ingredient = await Ingredient.findByIdAndRemove({
