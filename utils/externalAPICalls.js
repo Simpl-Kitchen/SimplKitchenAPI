1c9383a7a45ed1eeb7a869797fa372aacfcb9092
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-02-13 16:14:24 -0600

    Deleted comment

--- utils/externalAPICalls.js
+++ utils/externalAPICalls.js
@@ -3,33 +3,6 @@ require('dotenv').config()
 const axios = require("axios");
 
 
-// const ingredientAPICall = async (queryObject) => {
-    
-//     const options = {
-//         method: 'GET',
-//         url: process.env.URL,
-//         params: queryObject,
-//         headers: {
-//             'X-RapidAPI-Key': process.env.X_RAPIDAPI_KEY,
-//             'X-RapidAPI-Host': process.env.X_RAPIDAPI_HOST
-//         }
-//     };
-
-//     const searchResults = axios.request(options).then(function (response) {
-//         const data = []
-//         foodData = response.data.hints
-//         for (let i = 0; i < foodData.length; i++) {
-//             const { food } = response.data.hints[i]
-//             data.push(food)
-//         }
-//         return data
-//     }).catch(function (error) {
-//         console.error(error);
-//     });
-
-//     return searchResults
-// }
-
 const ingredientAPICall = async (queryObject) => {
     
     const options = {
