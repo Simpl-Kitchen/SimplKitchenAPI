26b9ec83e4610a213d10a32c1a7ad366039eba72
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-02-13 16:56:40 -0600

    Changed ingredient api logic

--- utils/externalAPICalls.js
+++ utils/externalAPICalls.js
@@ -3,6 +3,33 @@ require('dotenv').config()
 const axios = require("axios");
 
 
+// This version returns just the hints **
+// const ingredientAPICall = async (queryObject) => {
+    
+//     const options = {
+//                 method: 'GET',
+//                 url:  'https://api.edamam.com/api/food-database/v2/parser',
+//                 params: queryObject,
+//             };
+    
+
+//     const searchResults = axios.request(options).then(function (response) {
+//          const data = []
+//          console.log(response.data);
+//          foodData = response.data.hints
+//          for (let i = 0; i < foodData.length; i++) {
+//              const { food } = response.data.hints[i]
+//              data.push(food)
+//          }
+//          return data
+//      }).catch(function (error) {
+//          console.error(error);
+//      });
+
+//     return searchResults
+// }
+
+// This returns all the data ** 
 const ingredientAPICall = async (queryObject) => {
     
     const options = {
@@ -11,15 +38,8 @@ const ingredientAPICall = async (queryObject) => {
                 params: queryObject,
             };
     
-
     const searchResults = axios.request(options).then(function (response) {
-         const data = []
-         foodData = response.data.hints
-         for (let i = 0; i < foodData.length; i++) {
-             const { food } = response.data.hints[i]
-             data.push(food)
-         }
-         return data
+         return response.data
      }).catch(function (error) {
          console.error(error);
      });
