130f65c4754a0451bbf83f4ad81fc16c1ed336c2
Zenon-Nowakowski <70614514+Zenon-Nowakowski@users.noreply.github.com>
2023-02-17 10:22:39 -0600

    Update pantryRouter.js

--- routes/pantryRouter.js
+++ routes/pantryRouter.js
@@ -18,7 +18,7 @@ const {
 
 router.route('/').post(addIngredient).get(getAllIngredients)
 router.route('/:id').get(getIngredient).delete(deleteIngredient).patch(updateIngredient)
-router.route('/Recipe').post(addRecipe).get(getAllRecipes)
-router.route('/Recipe/:id').get(getRecipe).delete(deleteRecipe).patch(updateRecipe)
+router.route('/recipe').post(addRecipe).get(getAllRecipes)
+router.route('/recipe/:id').get(getRecipe).delete(deleteRecipe).patch(updateRecipe)
 
 module.exports = router
