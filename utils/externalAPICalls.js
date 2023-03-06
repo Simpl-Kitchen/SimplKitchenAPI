60867576d9addcd6e725bb347692621ed494b6ce
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-03-06 15:16:57 -0600

    Deleted comment

--- utils/externalAPICalls.js
+++ utils/externalAPICalls.js
@@ -8,33 +8,6 @@ const { search } = require('superagent');
 const connectSpoonacularApi = require('../connections/connectSpoonacular')
 const api = connectSpoonacularApi(process.env.SPOONACULAR_API_KEY)
 
-
-// This version returns just the hints **
-// const ingredientAPICall = async (queryObject) => {
-
-//     const options = {
-//                 method: 'GET',
-//                 url:  'https://api.edamam.com/api/food-database/v2/parser',
-//                 params: queryObject,
-//             };
-
-
-//     const searchResults = axios.request(options).then(function (response) {
-//          const data = []
-//          console.log(response.data);
-//          foodData = response.data.hints
-//          for (let i = 0; i < foodData.length; i++) {
-//              const { food } = response.data.hints[i]
-//              data.push(food)
-//          }
-//          return data
-//      }).catch(function (error) {
-//          console.error(error);
-//      });
-
-//     return searchResults
-// }
-
 // This returns all the data ** 
 
 // const ingredientAPICall = async (queryObject) => {
