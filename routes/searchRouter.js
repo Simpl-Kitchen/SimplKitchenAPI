846d90111223172d85c0ed444cc68d215004b82b
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-03-07 10:11:10 -0600

    Set up route for searchGroceryProductInformation

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -14,6 +14,7 @@ router.get('/ingredients', searchIngredients)
 router.get('/ingredient/properties/:id', searchIngredientInformation)
 
 router.get('/grocery/products', searchGroceryProducts)
+router.get('/grocery/product/properties', searchGroceryProductInformation)
 
 router.get('/recipes', searchRecipes)
 router.get('/pantryRecipe', searchByPantry)
