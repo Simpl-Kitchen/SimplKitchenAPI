84b8d32f9380413903d8b7840c3b0ef68281cab1
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-03-22 16:59:27 -0500

    Comments

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -3,13 +3,16 @@ const router = express.Router()
 
 const searchController = require('../controllers/searchControllers')
 
+// Ingredient search
 router.get('/ingredients', searchController.searchIngredients)
 router.get('/ingredient/id/:id', searchController.searchIngredientInformation)
 
+// Grocery search
 router.get('/grocery/products', searchController.searchGroceryProducts)
 router.get('/grocery/product/id/:id', searchController.searchGroceryProductInformation)
 router.get('/grocery/product/upc/:upc', searchController.searchGroceryProductByUPC)
 
+// Recipe search
 router.get('/recipes', searchController.searchRecipes)
 
 module.exports = router
