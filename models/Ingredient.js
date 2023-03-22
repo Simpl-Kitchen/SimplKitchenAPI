227d1e26bedd2710a777bb16ac736af24e0dcbf2
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-03-22 15:30:08 -0500

    Deleted comment

--- models/Ingredient.js
+++ models/Ingredient.js
@@ -1,54 +1,5 @@
 const mongoose = require('mongoose')
 
-// const IngredientSchema = new mongoose.Schema({
-//     foodId: {
-//         type: String,
-//         //required: [true, 'please provide the food Id'],
-//     },
-//     uri: {
-//         type: String,
-//     },
-//     label: {
-//         type: String,
-//         required: [true, 'please provide the name of the ingredient']
-
-//     },
-//     nutrients: {
-//         type: Map,
-//         of: String
-//     },
-//     brand: {
-//         type: String,
-
-//     },
-//     category: {
-//         type: String,
-//         enum: ['generic-foods', 'packaged-foods', 'generic-meals', 'fast-foods'],
-//     },
-//     categoryLabel: {
-//         type: String,
-//         enum: ['food', 'meal'],
-//         default: 'food'
-//     },
-//     foodContentsLabel: {
-//         type: String
-//     },
-//     image: {
-//         type: String,
-//     },
-//     amount: {
-//         type: Number
-//     },
-//     createdBy: {
-//         type: mongoose.Types.ObjectId,
-//         ref: 'User',
-//         required: [true, 'Please provide a user']
-//     }
-// },
-// {timestamps: true}, 
-// { strict: false },
-// ) // May need to change strict settings. Leaving as false for ease of testing. 
-
 const IngredientSchema = new mongoose.Schema({
     ingredientId: {
         type: String,
