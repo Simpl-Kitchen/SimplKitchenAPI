146425930330c3dd5c779412a1676a8fb9a806c7
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-03-22 20:13:41 -0500

    Basic recipe search

--- controllers/searchControllers/recipeSearchController.js
+++ controllers/searchControllers/recipeSearchController.js
@@ -7,32 +7,22 @@ const { BadRequestError, NotFoundError } = require('../../errors')
 
 const searchRecipes = async (req, res) => {
     // Set up variables. Destructure req.query and req.user
-    const {search} = req.query
-    const {userId} = req.user
+    const { search } = req.query
+    const { userId } = req.user
     const queryObject = {}
 
     if (!search) {
         throw new BadRequestError("No search terms provided")
     }
     // Construct query object
-
-    // if (!q) {
-    //     throw new BadRequestError("No search terms provided")
-    // } else if (!type) {
-    //     throw new BadRequestError("No Recipe type provided")
-    // } else {
-    //     queryObject.q = q
-    //     queryObject.type = type
-    // }
-
+    queryObject.search = search
     // Call ingredient API
     //recipeData = await externalAPICalls.searchRecipesAPI(queryObject)
     recipeData = await externalAPICalls.searchRecipesAPI(queryObject)
-    //recipeData = await recipeAPICall(queryObject)
+    //console.log(recipeData)
 
     // Return data to frontend
-    //res.status(StatusCodes.OK).json({ recipeData })
-    res.send("Hello World")
+    res.status(StatusCodes.OK).json({ recipeData })
 }
 
 module.exports = {
