f10e9f25e06829e395aa8e1d4f2725d30fd8cbd4
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-04-17 13:22:10 -0500

    Added Comments

--- controllers/generateController.js
+++ controllers/generateController.js
@@ -18,39 +18,30 @@ const generateNewQueue = async (req, res) => {
     const { userId } = req.user
     //const queryObject = {}
     
-    // // Get user intolerances
+    // // Get user intolerances and ingredients in the user's pantry
     const intolerances = await userHelpers.getUserIntolerances(userId)
     const ingredients = await userHelpers.getUserIngredients(userId)
 
+    // set up queryObject
     queryObject.intolerances = intolerances
     queryObject.ingredients = ingredients
     queryObject.number = 20
     queryObject.random = true
     queryObject.userId = req.user.userId
 
-    //const recipeData = await externalAPICalls.searchRecipesByIngredientsAPI(queryObject)
-
+    // Fill the queue according to the queryObject
     await fillQueue(queryObject)
 
-    // await Promise.all(recipeData.map(async (recipe) => {
-    //     recipe.createdBy = req.user.userId
-    //     await RecipeQueue.create(recipe);
-    // }));
-
-    //console.log()
-    
-
+    // Send message back to the frontend
     res.status(StatusCodes.OK).json({ msg: "Queue generated" })
-
-    //res.send("Ok")
-
 
 }
 const getQueue = async (req, res) => {
     queryObject = {
         createdBy: req.user.userId
     }
-    //let result = RecipeQueue.find(queryObject)
+
+
     let result = RecipeQueue.find(queryObject).sort({ createdAt: 1 }); // Add .sort({ createdAt: 1 }) here
     const recipes = await result
 
@@ -58,71 +49,48 @@ const getQueue = async (req, res) => {
     console.log("Inside if statement, count == ", count)
 
     res.status(StatusCodes.OK).json({ recipes })
-    // console.log("Hello from getQueue")
-    // res.send("Ok")
 
 }
 const removeFromQueue = async (req, res) => {
 
+    // Get user ID
     const { userId } = req.user
     //const queryObject = {}
     
-    // // Get user intolerances
+    // Get user intolerances and ingredients in their pantry
     const intolerances = await userHelpers.getUserIntolerances(userId)
     const ingredients = await userHelpers.getUserIngredients(userId)
 
     queryObject.intolerances = intolerances
     queryObject.ingredients = ingredients
-    //queryObject.number = 20
     queryObject.random = true
     queryObject.userId = userId
 
-    //console.log(req.params.id)
     console.log("Hello from removeFromQueue")
-    //res.send("Ok")
 
+    // Find the recipe to delete by Mongo DB _id
     let recipe = await RecipeQueue.findOne({
         _id: req.params.id,
         //createdBy: req.user.userId
     })
 
+    // If recipe doesn't exitst send a message
     if (!recipe) {
         throw new NotFoundError(`No recipe in the queue with id ${req.params.id}`)
     }
 
-
+    // Remove the recipe
     await recipe.remove();
+
+    // Fill the recipe queue
     await fillQueue(queryObject);
 
     res.status(StatusCodes.OK).send("Deleted")
 
-    //res.status(StatusCodes.OK).json( { newRecipeData })
 }
-// const addToQueue = async (req, res) => {
-//     console.log("Hello from addToQueue")
-
-
-//     const intolerances = await userHelpers.getUserIntolerances(userId)
-//     const ingredients = await userHelpers.getUserIngredients(userId)
-
-//     queryObject.intolerances = intolerances
-//     queryObject.ingredients = ingredients
-//     queryObject.number = 1
-//     queryObject.random = true
-
-//     const recipeData = await externalAPICalls.searchRecipesByIngredientsAPI(queryObject)
-
-//     await Promise.all(recipeData.map(async (recipe) => {
-//         recipe.createdBy = req.user.userId
-//         await RecipeQueue.create(recipe);
-//     }));
-
-//     //res.status(StatusCodes.OK).json({ recipeData })
-// }
 
 module.exports = {
     generateNewQueue,
     removeFromQueue,
-    //addToQueue,
     getQueue
 }
