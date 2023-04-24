ff3ec93425c801dd085758d025d7d076875c9252
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-04-24 13:54:40 -0500

    Shopping list schema v1

---
+++ models/ShoppingList.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const estimatedCostSchema = new mongoose.Schema({
+    value: Number,
+    unit: String
+  }); 
+
+
+  const ingredientSchema = new mongoose.Schema({
+    ingredientID: Number,
+    ingredientName: String,
+    amount: Number,
+    unit: String,
+    estimatedCost: estimatedCostSchema,
+    aisle: String,
+    image: String,
+});
+
+const ShoppingListSchema = new mongoose.Schema({
+    ingredient: [ingredientSchema],
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+    },
+    cost: Number,        
+    
+})
+
+ShoppingListSchema.methods.getTotalCost = function() {
+    let totalCost = 0;
+    
+    this.ingredients.forEach(ingredient => {
+        totalCost += ingredient.estimatedCost.value;
+    });
+
+    return totalCost;
+};
+
+ShoppingListSchema.pre('save', function(next) {
+    this.cost = this.getTotalCost();
+    next();
+})
+
+const ShoppingList = mongoose.model('ShoppingList', ShoppingListSchema);
+module.exports = ShoppingList;
