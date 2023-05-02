d3b0818a95683ad327767ecbfecbafbe82e474a6
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-05-02 15:56:59 -0500

    Comments

--- utils/helpers/cost.js
+++ utils/helpers/cost.js
@@ -1,22 +1,21 @@
-const axios = require('axios');
-//const { ingredientInformationAPICall } = require('../spoonacular/externalAPICalls')
 const { ingredientInformationAPICall } = require('../spoonacular/externalAPICalls')
 
 const calculateIngredientCost = async (ingredient) => {
 
+    // Construct query object for ingredientInformationAPICall
     queryObject = {}
     queryObject.id = ingredient.id
     queryObject.amount = ingredient.amount
     queryObject.unit = ingredient.unit
 
-
+    // Call ingredientInformationAPICall
     const results = await ingredientInformationAPICall(queryObject)
 
+    // Extract cost from results
     const cost = results.estimatedCost
     return cost
 }
 
 module.exports = {
-    //convertIngredientAmountToGrams,
     calculateIngredientCost
 }
