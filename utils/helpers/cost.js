94244a084e4e4220f4dcedeb75b1db1fbe72445d
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-05-02 11:31:37 -0500

    New and improved cost calculation

--- utils/helpers/cost.js
+++ utils/helpers/cost.js
@@ -2,58 +2,22 @@ const axios = require('axios');
 //const { ingredientInformationAPICall } = require('../spoonacular/externalAPICalls')
 const { ingredientInformationAPICall } = require('../spoonacular/externalAPICalls')
 
-// const convertIngredientAmountToGrams = async (name, amount, unit) => {
-
-//     //console.log(process.env.SPOONACULAR_API_KEY)
-
-//     let requestHeaders = {
-//         'x-api-key': process.env.SPOONACULAR_API_KEY
-//     }
-
-//     let opts = {
-//         ingredientName: name,
-//         sourceAmount: amount,
-//         sourceUnit: unit,
-//         targetUnit: 'grams'
-//     }
-
-//     const options = {
-//         method: 'GET',
-//         headers: requestHeaders,
-//         //url: 'https://api.spoonacular.com/recipes/convert?ingredientName=flour&sourceAmount=2.5&sourceUnit=cups&targetUnit=grams',
-//         url: 'https://api.spoonacular.com/recipes/convert',
-//         params: opts,
-//     };
-
-//     const searchResults = axios.request(options).then(function (response) {
-//         return response.data
-//     }).catch(function (error) {
-//         console.error(error);
-//     });
-
-//     //console.log(searchResults)
-
-//     return searchResults
-// }
-
 const calculateIngredientCost = async (ingredient) => {
 
-    // console.log("Ingredient ID ::", ingredient.ingredientID)
-    // console.log("Ingredient name::", ingredient.ingredientName)
-    // console.log("Amount in Recipe::", ingredient.amount, ingredient.unit)
-    // console.log("--------------------")
     // First calculate amount in grams
     let requestHeaders = {
         'x-api-key': process.env.SPOONACULAR_API_KEY
     }
 
+    // Set up params for convert api call
     let opts = {
-        ingredientName: ingredient.ingredientName,
+        ingredientName: ingredient.originalName,
         sourceAmount: ingredient.amount,
         sourceUnit: ingredient.unit,
         targetUnit: 'grams'
     }
 
+    // Set up options for the axios call
     const options = {
         method: 'GET',
         headers: requestHeaders,
@@ -62,19 +26,24 @@ const calculateIngredientCost = async (ingredient) => {
         params: opts,
     };
 
+    // Convert ingredient amount to grams with this api call
     const amountConversion = await axios.request(options).then(function (response) {
         return response.data
     }).catch(function (error) {
-        console.error(error);
+        console.error(error.message);
     });
 
-    //console.log(amountConversion)
+    // Now we have the amount of the ingredient in grams
     const amountInGrams = amountConversion.targetAmount
+
+    // Debuging stuff
+    console.log("Ingredient name :: ", ingredient.originalName) 
+    console.log("Ingredient original units ::", ingredient.unit)
     console.log("Amount In Grams ::", amountInGrams)
 
     // Call ingredient information API to get weight per serving in grams and estimated cost
     queryObject = {}
-    queryObject.id = ingredient.ingredientID
+    queryObject.id = ingredient.id
     const ingredientInformation = await ingredientInformationAPICall(queryObject)
 
 
@@ -82,23 +51,32 @@ const calculateIngredientCost = async (ingredient) => {
     const weightPerServing = ingredientInformation.nutrition.weightPerServing.amount
     const estimatedCost = ingredientInformation.estimatedCost.value
 
-    // Calculate cost per gram and the total cost (in cents)
-    const costPerGram = estimatedCost / (weightPerServing == 0 ? 1 : weightPerServing)
-    const totalCost = costPerGram * amountInGrams
 
+    // Calculate cost per gram and the total cost (in cents)
+    let costPerGram;
+    let totalCost;
+
+    // Some of the ingredient don't come with weight information so I had to set those to 0
+    if (!weightPerServing){
+        costPerGram = 0
+        totalCost = 0
+    }
+    // For those that do have that information calculate cost per gram and total cost
+    else {
+
+        costPerGram = estimatedCost / weightPerServing 
+        totalCost = costPerGram * amountInGrams
+    }
+
+    // Create a cost object
     const cost = {
         costPerGram: costPerGram,
         totalCost: totalCost
     }
 
+    console.log("Total cost in cents ::", cost.totalCost)
+    // And return it
     return cost
-
-
-
-
-
-    //return searchResults
-
 }
 
 module.exports = {
